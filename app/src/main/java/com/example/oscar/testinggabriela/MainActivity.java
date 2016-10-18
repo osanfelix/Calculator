a73116610e44802e7a71f4f48ee86465fb3a595c
Oscar <osanfelix@outlook.es>
2016-10-18 22:44:41 +0200

    Solo la pantalla

---
+++ app/src/main/java/com/example/oscar/testinggabriela/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.oscar.testinggabriela;
+
+import android.app.Application;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    // Button functions
+    public void click0(View view)
+    {
+        TextView label = (TextView)findViewById(R.id.textView);
+        label.setText("1");
+    }
+
+    public void click1(View view)
+    {
+
+    }
+
+    public void click2(View view)
+    {
+
+    }
+
+    public void click3(View view)
+    {
+
+    }
+
+    public void click4(View view)
+    {
+
+    }
+
+    public void click5(View view)
+    {
+
+    }
+
+    public void click6(View view)
+    {
+
+    }
+
+    public void click7(View view)
+    {
+
+    }
+
+    public void click8(View view)
+    {
+
+    }
+
+    public void click9(View view)
+    {
+
+    }
+
+}
