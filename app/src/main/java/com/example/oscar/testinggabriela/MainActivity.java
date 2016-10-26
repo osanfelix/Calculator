ba29e7080577bf2c67aa1372527a952fa982eb10
Oscar <osanfelix@outlook.es>
2016-10-26 18:25:30 +0200

    En teoria sólo suma, sin pulsar '='

--- app/src/main/java/com/example/oscar/testinggabriela/MainActivity.java
+++ app/src/main/java/com/example/oscar/testinggabriela/MainActivity.java
@@ -11,13 +11,13 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
-
 public class MainActivity extends AppCompatActivity {
+    enum OPERATION{ADD, SUB, MULT, DIV}
+
     protected TextView _label = null;
+
+    protected int _lastOperator = 0;
+    protected OPERATION _lastOperation = OPERATION.ADD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,83 +30,102 @@ public class MainActivity extends AppCompatActivity {
 
 
     // Button functions
-    public void click0(View view) {
-        _label.setText("1");
+    public void click0(View view)
+    {
+        _label.append("0");
     }
 
-    public void click1(View view) {
-
+    public void click1(View view)
+    {
+        _label.append("1");
     }
 
-    public void click2(View view) {
-
+    public void click2(View view)
+    {
+        _label.append("2");
     }
 
-    public void click3(View view) {
-
+    public void click3(View view)
+    {
+        _label.append("3");
     }
 
-    public void click4(View view) {
-
+    public void click4(View view)
+    {
+        _label.append("4");
     }
 
-    public void click5(View view) {
-
+    public void click5(View view)
+    {
+        _label.append("5");
     }
 
-    public void click6(View view) {
-
+    public void click6(View view)
+    {
+        _label.append("6");
     }
 
-    public void click7(View view) {
-
+    public void click7(View view)
+    {
+        _label.append("7");
     }
 
-    public void click8(View view) {
-
+    public void click8(View view)
+    {
+        _label.append("8");
     }
 
-    public void click9(View view) {
-
+    public void click9(View view)
+    {
+        _label.append("9");
     }
 
-    public void clickPlus(View view) {
-
-    }
-
-    public void clickAdd(View view) {
-
-    }
-
-    public void clickMinus(View view) {
-
-    }
-
-    public void clickDelete(View view) {
-
-    }
-
-    public void clickDiv(View view) {
+    public void clickMult(View view)
+    {
 
     }
 
-    public void clickEquals(View view) {
+    public void clickAdd(View view)
+    {
+        int result = 0;
+        switch (_lastOperation)
+        {
+            case ADD:
+                result = _lastOperator + Integer.parseInt(_label.getText().toString());
+                break;
+            case SUB:
+                result = _lastOperator - Integer.parseInt(_label.getText().toString());
+                break;
+            case MULT:
+                result = _lastOperator * Integer.parseInt(_label.getText().toString());
+                break;
+            case DIV:
+                result = _lastOperator / Integer.parseInt(_label.getText().toString());
+                break;
+        }
+        _label.setText( Integer.toString(result));
+
+        _lastOperator = result;
+        _lastOperation = OPERATION.ADD;
+    }
+
+    public void clickSub(View view)
+    {
 
     }
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("Main Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
+    public void clickDel(View view)
+    {
+
+    }
+
+    public void clickDiv(View view)
+    {
+
+    }
+
+    public void clickEquals(View view)
+    {
+
     }
 }
